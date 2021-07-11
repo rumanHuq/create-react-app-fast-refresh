cf72314e8f695a1a7833b84f2c1be4daa223d848
Ruman <rumanbsl@gmail.com>
2021-07-11 12:26:48 +0300

    lockfile update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,183 +1,156 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const schemaJson = require('./schema.json')
+
+const development = process.env.STAGE !== 'prod'
+const devRules = !development ? {'no-console': 'warn'} : {'no-console': 0}
+const rules = {
+  '@typescript-eslint/indent': 0,
+  '@typescript-eslint/ban-ts-comment': 0,
+  '@typescript-eslint/camelcase': 0,
+  '@typescript-eslint/explicit-module-boundary-types': 0,
+  '@typescript-eslint/no-unused-vars': ['error'],
+  '@typescript-eslint/no-unused-expressions': ['error'],
+  '@typescript-eslint/no-explicit-any': 0,
+  '@typescript-eslint/no-misused-promises': 0,
+  '@typescript-eslint/no-object-literal-type-assertion': 0,
+  '@typescript-eslint/no-this-alias': 0,
+  '@typescript-eslint/no-unsafe-assignment': 0,
+  '@typescript-eslint/no-unsafe-call': 0,
+  '@typescript-eslint/no-unsafe-member-access': 0,
+  '@typescript-eslint/no-unsafe-return': 0,
+  '@typescript-eslint/require-await': 0,
+  '@typescript-eslint/restrict-template-expressions': 0,
+  'import/extensions': 0,
+  'import/no-cycle': 0,
+  'import/no-named-as-default': 2,
+  'import/no-anonymous-default-export': 2,
+  'import/order': [
+    'error',
+    {
+      alphabetize: {order: 'asc', caseInsensitive: false},
+      pathGroups: [
+        {
+          pattern: '@/**',
+          group: 'external',
+          position: 'after',
+        },
+      ],
+      pathGroupsExcludedImportTypes: ['builtin'],
+    },
+  ],
+  indent: 'off',
+  'jsx-a11y/anchor-is-valid': 0,
+  'no-plusplus': 0,
+  'no-param-reassign': 0,
+  'no-throw-literal': 0,
+  'no-underscore-dangle': 0,
+  'no-unused-expressions': 0,
+  'no-unused-vars': 0,
+  'no-void': 0,
+  'prettier/prettier': 'error',
+  'require-jsdoc': [
+    'error',
+    {
+      require: {
+        ArrowFunctionExpression: false,
+        ClassDeclaration: true,
+        FunctionDeclaration: false,
+        FunctionExpression: false,
+        MethodDefinition: false,
+      },
+    },
+  ],
+  'graphql/template-strings': [
+    'error',
+    {
+      env: 'apollo',
+      schemaJson,
+    },
+  ],
+  'react/jsx-filename-extension': [
+    2,
+    {
+      extensions: ['.tsx', '.jsx'],
+    },
+  ],
+  'react/jsx-props-no-spreading': 0,
+  'react/no-array-index-key': 0,
+  'react/prop-types': 0,
+  'react-hooks/exhaustive-deps': 0,
+  'react/react-in-jsx-scope': 'off',
+  'react/jsx-one-expression-per-line': 0,
+  'react/require-default-props': 0,
+  ...devRules,
+}
+
+const testRules = {
+  ...rules,
+  '@typescript-eslint/no-var-requires': 0,
+  '@typescript-eslint/unbound-method': 0,
+  '@typescript-eslint/no-unused-expressions': 0,
+  '@typescript-eslint/no-explicit-any': 0,
+  'import/no-extraneous-dependencies': 0,
+  'import/prefer-default-export': 0,
+  'require-jsdoc': 0,
+}
 
 module.exports = {
+  root: true,
   env: {
-    browser : true,
-    es6     : true,
-    jest    : true,
+    browser: true,
+    es6: true,
   },
-  extends: [
-    "react-app",
-    "airbnb",
-    "airbnb/hooks",
-    "plugin:@typescript-eslint/recommended"
-  ],
-  rules: {
-    "@typescript-eslint/member-ordering": [
-      2,
-      { default: ["signature", "method", "constructor", "field"] }
-    ],
-    "no-use-before-define"                                : 0,
-    "@typescript-eslint/no-use-before-define"             : "error",
-    "@typescript-eslint/ban-ts-ignore"                    : "off",
-    "@typescript-eslint/camelcase"                        : "off",
-    "@typescript-eslint/explicit-function-return-type"    : "off",
-    "@typescript-eslint/indent"                           : "off",
-    "@typescript-eslint/no-explicit-any"                  : "off",
-    "@typescript-eslint/no-object-literal-type-assertion" : "off",
-    "@typescript-eslint/no-this-alias"                    : "off",
-    camelcase                                             : 0,
-    "comma-dangle"                                        : 0,
-    "comma-spacing"                                       : [
-      "error",
-      {
-        after  : true,
-        before : false
-      }
-    ],
-    "func-names"                 : 0,
-    "import/extensions"          : 0,
-    "import/no-cycle"            : 0,
-    "import/no-named-as-default" : 0,
-    "import/no-unresolved"       : [
-      2,
-      { caseSensitive: true }
-    ],
-    indent: [
-      "error",
-      2,
-      { SwitchCase: 1 }
-    ],
-    "jsx-a11y/anchor-is-valid" : 0,
-    "key-spacing"              : [
-      "error",
-      {
-        align: {
-          afterColon  : true,
-          beforeColon : true,
-          on          : "colon"
-        }
-      }
-    ],
-    "linebreak-style"             : "error",
-    "lines-between-class-members" : 0,
-    "max-len"                     : [
-      "error",
-      { code: 160 }
-    ],
-    "no-multi-spaces"         : 2,
-    "no-multiple-empty-lines" : [
-      "error",
-      {
-        max    : 1,
-        maxEOF : 0
-      }
-    ],
-    "no-param-reassign"    : 0,
-    "no-throw-literal"     : 0,
-    "no-trailing-spaces"   : "error",
-    "no-underscore-dangle" : 0,
-    "no-unused-vars"       : 0,
-    "object-curly-newline" : [
-      "error",
-      { multiline: true }
-    ],
-    "object-curly-spacing": [
-      "error",
-      "always"
-    ],
-    quotes: [
-      "error",
-      "double"
-    ],
-    "react/jsx-filename-extension": [
-      2,
-      {
-        extensions: [
-          ".tsx",
-          ".jsx"
-        ]
-      }
-    ],
-    "import/prefer-default-export" : 0,
-    "import/no-default-export"     : 2,
-    "react/jsx-props-no-spreading" : 0,
-    "react/no-array-index-key"     : 0,
-    "react/prop-types"             : 0,
-    "react/react-in-jsx-scope"     : "off",
-    "require-jsdoc"                : [
-      "error",
-      {
-        require: {
-          ArrowFunctionExpression : false,
-          ClassDeclaration        : true,
-          FunctionDeclaration     : true,
-          FunctionExpression      : false,
-          MethodDefinition        : false
-        }
-      }
-    ],
-    semi                  : "error",
-    "space-before-blocks" : [
-      "error",
-      {
-        classes   : "never",
-        functions : "always",
-        keywords  : "always"
-      }
-    ]
-  },
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {project: './tsconfig.json'},
+  plugins: ['import', 'graphql'],
   settings: {
-    "import/parsers": {
-      "@typescript-eslint/parser": [
-        ".ts",
-        ".tsx"
-      ]
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
-    "import/resolver": {
+    'import/resolver': {
       alias: {
-        extensions: [
-          ".ts",
-          ".js",
-          ".tsx",
-          ".jsx",
-          ".json"
-        ],
-        map: [
-          [
-            "@",
-            "./src/"
-          ]
-        ]
+        extensions: ['.ts', '.js', '.tsx', '.jsx', '.json'],
+        map: [['@', './src/']],
       },
-      "eslint-import-resolver-typescript" : true,
-      node                                : true,
-      webpack                             : {
-        config: {
-          resolve: {
-            extensions: [
-              ".ts",
-              ".js",
-              ".tsx",
-              ".jsx"
-            ]
-          }
-        }
-      }
-    }
+      'eslint-import-resolver-typescript': true,
+    },
   },
+  rules,
   overrides: [
     {
-      files: [
-        "*.{test,spec,story}.{js,ts,jsx,tsx}"
+      files: './cypress/**/*.ts',
+      parserOptions: {
+        project: './cypress/tsconfig.json',
+        ecmaFeatures: {
+          jsx: true,
+          spread: true,
+        },
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:prettier/recommended',
+        'plugin:cypress/recommended',
       ],
-      rules: {
-        "require-jsdoc"                                    : 0,
-        "@typescript-eslint/explicit-function-return-type" : 0,
-        "@typescript-eslint/no-explicit-any"               : 0,
-        "@typescript-eslint/camelcase"                     : 0,
-        "@typescript-eslint/no-unused-vars"                : 0,
-        "import/no-extraneous-dependencies"                : 0
-      }
-    }
-  ]
-};
+      plugins: ['cypress'],
+      env: {
+        'cypress/globals': true,
+        node: true,
+      },
+      rules: testRules,
+    },
+    {
+      files: ['*.config.{js,ts}', './src/**/*.{test,spec}.{tsx,ts}'],
+      rules: testRules,
+      env: {
+        browser: true,
+        es6: true,
+        jest: true,
+        node: true,
+      },
+    },
+  ],
+}
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  tabs: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  bracketSpacing: false,
+  printWidth: 180,
+}
